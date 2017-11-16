87a9a47e934896343ba247da4663a67e766f801e
mdmitriy <dmitriy.mihaylichenko@valor-software.com>
2017-11-16 15:10:47 +0200

    fix(embed): fix embed set

--- src/embed/embed.controller.ts
+++ src/embed/embed.controller.ts
@@ -6,41 +6,39 @@ import { ObjectID } from 'mongodb';
 const Embed = mongoose.model('Embed');
 
 module.exports = (app: any) => {
-  app.get('/v1/get-index-mock',
-    getIndexMock);
+  app.get('/show-embed-set',
+    showEmbedSet);
 
-    function getIndexMock(): Function {
-      return (req: any, res: any) => {
-        const query = req.query;
-        const embedId = query.embed;
+    function showEmbedSet(req: any, res: any) {
+      const query = req.query;
+      const embedId = query.embed;
 
-        Embed
-          .findOne({_id: embedId})
-          .lean()
-          .exec((err: any, data: any) => {
-            if (err) {
-              return res.json({success: !err, msg: [], data: null, error: err});
-            }
+      Embed
+        .findOne({_id: embedId})
+        .lean()
+        .exec((err: any, data: any) => {
+          if (err) {
+            return res.json({success: !err, msg: [], data: null, error: err});
+          }
 
-            if (req.headers.referer) {
-              res.redirect(`${data.targetUrl}`);
-              return;
-            }
+          if (req.headers.referer) {
+            res.redirect(`${data.targetUrl}`);
+            return;
+          }
 
-            res.writeHeader(200, {"Content-Type": "text/html"});
-            let html = `
-              <!DOCTYPE html>
-              <html lang="en" prefix="og: http://ogp.me/ns#">
-              <head>
-                <meta property="og:image" content="${data.imageUrl}">
-                <meta property="og:title" content="Dollar Street">
-                <meta property="og:description" content="See how people really live">
-                <meta property="og:type" content="article">
-              </head>
-            `;
-            res.write(html);
-            res.end();
-          });
-      }
+          res.writeHeader(200, {"Content-Type": "text/html"});
+          let html = `
+            <!DOCTYPE html>
+            <html lang="en" prefix="og: http://ogp.me/ns#">
+            <head>
+              <meta property="og:image" content="${data.imageUrl}">
+              <meta property="og:title" content="Dollar Street">
+              <meta property="og:description" content="See how people really live">
+              <meta property="og:type" content="article">
+            </head>
+          `;
+          res.write(html);
+          res.end();
+        });
     }
 };
